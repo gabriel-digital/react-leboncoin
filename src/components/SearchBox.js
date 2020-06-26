2839cef10f492fb8f9083a709ddd375c926e54d3
gabriel-digital <gabrielfetu@gmail.com>
2020-06-26 09:27:00 +0200

    filters implemented

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,29 +1,86 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
-const SearchBox = ({ query, setQuery }) => {
+const SearchBox = ({ setData, setLoading, query, setQuery }) => {
+  let url = `${process.env.REACT_APP_ENV}/offers/with-count`;
+  const setUrl = () => {
+    if (!query.title) {
+      url = `${process.env.REACT_APP_ENV}/offers/with-count`;
+    } else {
+      url = `${process.env.REACT_APP_ENV}/offers/with-count?title=${query.title}&priceMin=${query.priceMin}&priceMax=${query.priceMax}&sort=${query.sort}`;
+    }
+    return url;
+  };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.response.data.error.message);
+    }
+  };
   return (
     <section className="search">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setUrl(query);
+          console.log(query);
+          console.log(url);
+          fetchData();
+        }}
+      >
         <div className="mainSearch">
           <input
             type="text"
-            placeholder="Try something like 'cat', 'kitten, 'cut'..."
+            placeholder="Try something like 'cat', 'kitten, 'cute'"
+            value={query.title}
+            onChange={(event) => {
+              const obj = { ...query };
+              obj.title = event.target.value;
+              setQuery(obj);
+            }}
           />
           <input type="submit" value="Rechercher" className="action" />
         </div>
         <div className="filters">
           <div className="price">
             prix entre
-            <input type="text" placeholder="prix min" />
+            <input
+              type="text"
+              placeholder="prix min"
+              value={query.priceMin}
+              onChange={(event) => {
+                const obj = { ...query };
+                obj.priceMin = event.target.value;
+                setQuery(obj);
+              }}
+            />
             entre
-            <input type="text" placeholder="prix max" />
+            <input
+              type="text"
+              placeholder="prix max"
+              value={query.priceMax}
+              onChange={(event) => {
+                const obj = { ...query };
+                obj.priceMax = event.target.value;
+                setQuery(obj);
+              }}
+            />
           </div>
           <div className="sort">
-            <select>
-              <option selected> tri : Plus récentes</option>
-              <option selected> tri : Plus anciennes</option>
-              <option selected> tri : Plus chères</option>
-              <option selected> tri : Moins chères</option>
+            <select
+              onChange={(event) => {
+                const obj = { ...query };
+                obj.sort = event.target.value;
+                setQuery(obj);
+              }}
+            >
+              <option value="date-desc"> tri : Plus récentes</option>
+              <option value="date-asc"> tri : Plus anciennes</option>
+              <option value="price-desc"> tri : Plus chères</option>
+              <option value="price-asc"> tri : Moins chères</option>
             </select>
           </div>
         </div>
--- src/containers/Offers.js
+++ src/containers/Offers.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { formatDate } from '../utils';
 import Loader from '../components/Loader';
+import SearchBox from '../components/SearchBox';
 
 const Offers = () => {
   // init states
@@ -10,31 +11,15 @@ const Offers = () => {
   const [loading, setLoading] = useState(true);
   const [query, setQuery] = useState({});
 
-  let url = `${process.env.REACT_APP_ENV}/offers/with-count`;
-  const setUrl = () => {
-    if (!query.title) {
-      url = `${process.env.REACT_APP_ENV}/offers/with-count`;
-    } else {
-      url = `${process.env.REACT_APP_ENV}/offers/with-count?title=${query.title}`;
-    }
-    return url;
-  };
-
   // server request
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(url);
-      setData(response.data);
-      setLoading(false);
-    } catch (error) {
-      console.log(error.response.data.error.message);
-    }
-  };
+
   // call server request once
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(url);
+        const response = await axios.get(
+          `${process.env.REACT_APP_ENV}/offers/with-count`
+        );
         setData(response.data);
         setLoading(false);
       } catch (error) {
@@ -42,7 +27,7 @@ const Offers = () => {
       }
     };
     fetchData();
-  }, [url]);
+  }, []);
 
   return loading ? (
     <main>
@@ -51,43 +36,12 @@ const Offers = () => {
   ) : (
     <>
       <main className="offers">
-        <section className="search">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              setUrl(query);
-              fetchData();
-            }}
-          >
-            <div className="mainSearch">
-              <input
-                type="text"
-                placeholder="Try something like 'cat', 'kitten, 'cute'"
-                value={query.title}
-                onChange={(event) => {
-                  setQuery({ title: event.target.value });
-                }}
-              />
-              <input type="submit" value="Rechercher" className="action" />
-            </div>
-            {/* <div className="filters">
-              <div className="price">
-                prix entre
-                <input type="text" placeholder="prix min" />
-                entre
-                <input type="text" placeholder="prix max" />
-              </div>
-              <div className="sort">
-                <select>
-                  <option selected> tri : Plus récentes</option>
-                  <option selected> tri : Plus anciennes</option>
-                  <option selected> tri : Plus chères</option>
-                  <option selected> tri : Moins chères</option>
-                </select>
-              </div>
-            </div> */}
-          </form>
-        </section>
+        <SearchBox
+          setLoading={setLoading}
+          setData={setData}
+          query={query}
+          setQuery={setQuery}
+        />
 
         {data.offers.length === 0 ? (
           <section className="noresults">
