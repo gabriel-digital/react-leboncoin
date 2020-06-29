efabca694dbdbf14d0c868daff90cace4af7473b
gabriel-digital <gabrielfetu@gmail.com>
2020-06-29 14:40:06 +0200

    header responsive improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,19 +11,10 @@ const Header = ({ setUser, tokenFromCookie }) => {
   return (
     <header className={location === '/offers' ? 'isSearch' : ''}>
       <div className="container">
+        <Link className="home" to="/">
+          {logo(169, 30, '#F56B2A')}
+        </Link>
         <nav>
-          {/* <button className="navToggle">
-            <svg width="40px" height="40px" viewBox="0 0 20 14">
-              <path
-                d="M1.111 14H18.89C19.5 14 20 13.475 20 12.833c0-.641-.5-1.166-1.111-1.166H1.11C.5 11.667 0 12.192 0 12.833 0 13.475.5 14 1.111 14zm0-5.833H18.89C19.5 8.167 20 7.642 20 7s-.5-1.167-1.111-1.167H1.11C.5 5.833 0 6.358 0 7s.5 1.167 1.111 1.167zM0 1.167c0 .641.5 1.166 1.111 1.166H18.89C19.5 2.333 20 1.808 20 1.167 20 .525 19.5 0 18.889 0H1.11C.5 0 0 .525 0 1.167z"
-                fill="#f56b2a"
-                fillRule="evenodd"
-              ></path>
-            </svg>
-          </button> */}
-          <Link className="home" to="/">
-            {logo(169, 30, '#F56B2A')}
-          </Link>
           <Link className="publish" to="/offer/publish">
             {pictoPlus(23, 23, 'white')}
             <span>Déposer une annonce</span>
